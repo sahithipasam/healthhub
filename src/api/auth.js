949704ce3133f810fc1d9c034aa482d8031dc170
sahithi <sahithipasam06@gmail.com>
2026-04-28 14:48:10 +0530

    made some changes about backend deployment

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.DEV
   ? "/api"
-  : import.meta.env.VITE_API_BASE_URL || "http://localhost:2026";
+  : import.meta.env.VITE_API_BASE_URL || "https://backend-healthhub.vercel.app";
 
 const API = axios.create({
   baseURL: API_BASE_URL,
